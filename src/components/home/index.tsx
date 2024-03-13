dad9a3df686642077fa090b0b74ac260ad89ec06
Hamtana <hamtana.acc@gmail.com>
2024-03-13 05:46:39 -0700

    header v2

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,11 +7,12 @@ import Image from "next/image";
 
 export default function HomePage() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [categoryHover, setcategoryHover] = useState(0);
   return (
     <>
       <div className="flex flex-col justify-start min-w-full w-screen">
         <header className="flex w-full fixed top-0 left-0 rtl px-12">
-          <div className="flex flex-col border-b-2 w-full py-3">
+          <div className="flex flex-col border-b-2 border-[#888C8C] w-full py-3">
             <div className="flex justify-between items-center w-full px-2">
               <div className="flex items-center gap-5">
                 <h1 className="text-[#D1BBFF] text-3xl">We Gamers</h1>
@@ -93,16 +94,81 @@ export default function HomePage() {
                 )}
               </div>
             </div>
-            <div className="flex items-center pt-2 px-2">
+            <div className="flex items-center pt-3 px-2 gap-5">
+              <div className="flex flex-col relative">
+                <Link
+                  href=""
+                  onMouseEnter={() => setcategoryHover(1)}
+                  className="flex justify-center items-center rounded-2xl text-white hover:text-[#D1BBFF] gap-3 pl-4"
+                >
+                  <Icon
+                    icon="solar:hamburger-menu-line-duotone"
+                    className="w-7 h-7"
+                  />
+                  <span>دسته بندی ها</span>
+                </Link>
+                <div
+                  
+                  onMouseLeave={() => setcategoryHover(0)}
+                  className={
+                    categoryHover == 0
+                      ? " absolute z-10 top-12 hidden right-0 left-0  justify-around items-center p-2 text-indigo-600  bg-zinc-100  rounded-br-md rounded-bl-md transition-all duration-500 "
+                      : " absolute z-20 top-12 right-0 left-0 flex justify-around items-center p-2 text-indigo-600  bg-zinc-100  rounded-br-md rounded-bl-md transition-all duration-500 "
+                  }
+                >
+                  <h1>sdsd</h1>
+                </div>
+              </div>
               <Link
                 href=""
-                className="flex justify-center items-center rounded-2xl text-white gap-3"
+                className=" flex justify-center items-center rounded-2xl text-[#D1BBFF] hover:text-[#D1BBFF] gap-2"
               >
                 <Icon
-                  icon="solar:hamburger-menu-line-duotone"
-                  className="w-7 h-7"
+                  icon="solar:home-smile-angle-bold-duotone"
+                  className="w-6 h-6"
                 />
-                <span>دسته بندی ها</span>
+                صفحه اصلی
+              </Link>
+              <Link
+                href=""
+                className=" flex justify-center items-center rounded-2xl text-[#888C8C] hover:text-[#D1BBFF] gap-2"
+              >
+                <Icon icon="solar:shop-bold-duotone" className="w-6 h-6" />
+                فروشگاه
+              </Link>
+              <Link
+                href=""
+                className=" flex justify-center items-center rounded-2xl text-[#888C8C] hover:text-[#D1BBFF] gap-2"
+              >
+                <Icon icon="solar:cup-bold-duotone" className="w-6 h-6" />
+                تورنومنت
+              </Link>
+              <Link
+                href=""
+                className=" flex justify-center items-center rounded-2xl text-[#888C8C] hover:text-[#D1BBFF] gap-2"
+              >
+                <Icon icon="solar:bookmark-bold-duotone" className="w-6 h-6" />
+                وبلاگ
+              </Link>
+              <Link
+                href=""
+                className=" flex justify-center items-center rounded-2xl text-[#888C8C] hover:text-[#D1BBFF] gap-2"
+              >
+                <Icon
+                  icon="solar:phone-calling-bold-duotone"
+                  className="w-6 h-6"
+                />
+                تماس با ما
+              </Link>
+              <Link
+                href=""
+                className=" flex justify-center items-center rounded-2xl text-[#888C8C] hover:text-[#D1BBFF] gap-2"
+              >
+                <Icon
+                  icon="solar:info-square-bold-duotone"
+                  className="w-6 h-6"
+                />
+                درباره ما
               </Link>
             </div>
           </div>
