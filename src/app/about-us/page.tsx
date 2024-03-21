b215410821231a98999b85463b540c7375269bb8
Hamtana <hamtana.acc@gmail.com>
2024-03-21 02:17:55 -0700

    🚀 Deploy V 0.2.0

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import MainHeader from "@/components/base/headers/main-header";
-
 export default function AboutUs() {
   return (
     <main className="flex flex-col gap-3 pt-36">
-      <MainHeader />
       <div>درباره ما</div>
     </main>
   );
