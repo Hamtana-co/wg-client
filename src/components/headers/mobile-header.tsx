d65a0c6ecf6e2a10ffd754fa88ba981a77e7f98d
Hamtana <hamtana.acc@gmail.com>
2024-03-24 00:53:14 -0700

    🚀Deploy hamberger menu

--- src/components/headers/mobile-header.tsx
+++ src/components/headers/mobile-header.tsx
@@ -5,19 +5,11 @@ import { Icon } from "@iconify/react";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { CookiesProvider } from "react-cookie";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from "@nextui-org/react";
 
 export default function MobileHeader() {
   const [isHidden, setIsHidden] = useState(true);
   const [popUp, setPopUp] = useState(false);
+  const [hambergerMenu, sethambergerMenu] = useState(false);
 
   useEffect(() => {
     let lastScrollTop = 0;
@@ -47,8 +39,73 @@ export default function MobileHeader() {
       }
     >
       <div className="flex items-center">
-        <button>
+        <button onClick={() => sethambergerMenu(true)}>
           <Icon icon="solar:hamburger-menu-line-duotone" className="w-7 h-7" />
+          {hambergerMenu == true ? (
+            <div
+              onTouchStart={() => sethambergerMenu(false)}
+              className="absolute w-[100vw] bg-[#5b595eb9] h-screen top-0 right-0 z-40 "
+            >
+              <div className="bg-[#201f22] w-[60vw] h-screen  p-3 flex flex-col justify-start items-start gap-5">
+                <div className="flex justify-between items-center w-full border-b-2 pb-3">
+                  <h1 className="text-[#D1BBFF] text-xl">
+                    <Link href="/">We Gamers</Link>
+                  </h1>
+                  <button
+                    onTouchStart={() => sethambergerMenu(false)}
+                    onClick={() => sethambergerMenu(false)}
+                  >
+                    <Icon
+                      icon="solar:close-square-bold-duotone"
+                      className="w-8 h-8"
+                    />
+                  </button>
+                </div>
+                {/* <Link
+                  href="/register"
+                  className="flex justify-start items-center gap-2 w-full px-2"
+                >
+                  <Icon
+                    icon="solar:user-plus-bold-duotone"
+                    className="w-6 h-6"
+                  />
+                  قوانین سایت
+                </Link> */}
+                <Link
+                  href="/faq"
+                  className="flex justify-start items-center gap-2 w-full px-2"
+                >
+                  <Icon
+                    icon="solar:question-square-bold-duotone"
+                    className="w-6 h-6"
+                  />
+                  سوالات متداول
+                </Link>
+                <Link
+                  href="/contact-us"
+                  className="flex justify-start items-center gap-2 w-full px-2"
+                >
+                  <Icon
+                    icon="solar:phone-calling-bold-duotone"
+                    className="w-6 h-6"
+                  />
+                  تماس با ما
+                </Link>
+                <Link
+                  href="/about-us"
+                  className="flex justify-start items-center gap-2 w-full px-2"
+                >
+                  <Icon
+                    icon="solar:info-square-bold-duotone"
+                    className="w-6 h-6"
+                  />
+                  درباره ما
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
         </button>
       </div>
       <div className="flex items-center">
